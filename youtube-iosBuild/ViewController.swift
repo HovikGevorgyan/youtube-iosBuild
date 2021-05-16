efbfa24a5ced7eb94f60fcd0d59c9f980ebc8f95
Hovik Gevorgyan <hovik_gevorgyan@Hoviks-iMac.local>
2021-05-16 10:11:08 +0400

    initial xcode project creation

---
+++ youtube-iosBuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-iosBuild
+//
+//  Created by Hovik Gevorgyan on 16.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
